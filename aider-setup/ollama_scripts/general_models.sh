e72fa6d98de262f14ab425917a1b4195ce73bf5f
Your Name <you@example.com>
2026-02-02 18:31:59 -0800

    updates to model selections

--- aider-setup/ollama_scripts/general_models.sh
+++ aider-setup/ollama_scripts/general_models.sh
@@ -9,10 +9,10 @@ mkdir --parents $LOGFILES
 
 for model in \
   glm-4.7-flash:q4_K_M \
+  qwen3-vl:32b-thinking-q4_K_M \
   nemotron-3-nano:30b-a3b-q4_K_M \
   gpt-oss:20b \
   magistral:24b-small-2506-q4_K_M \
-  qwen3:30b-a3b-thinking-2507-q4_K_M \
   deepseek-r1:32b-qwen-distill-q4_K_M
 
 do
--- aider-setup/ollama_scripts/small_general_models.sh
+++ aider-setup/ollama_scripts/small_general_models.sh
@@ -8,9 +8,9 @@ export LOGFILES=$HOME/Logfiles
 mkdir --parents $LOGFILES
 
 for model in \
+  qwen3-vl:8b-thinking-q4_K_M \
   gpt-oss:20b \
   magistral:24b-small-2506-q4_K_M \
-  qwen3:14b-q4_K_M \
   deepseek-r1:14b-qwen-distill-q4_K_M
 
 do
