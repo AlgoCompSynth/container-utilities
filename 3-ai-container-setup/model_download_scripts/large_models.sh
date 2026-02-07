80088b6f9d1fa76d0b7e2bcd220ca8017bac21ab
Your Name <you@example.com>
2026-02-07 00:46:20 -0800

    cleanup

--- 3-ai-container-setup/model_download_scripts/large_models.sh
+++ 3-ai-container-setup/model_download_scripts/large_models.sh
@@ -2,36 +2,35 @@
 
 set -e
 
-echo "*** Large (>= 16 GB and <= 24 GB) Models ***"
+echo "*** Large (>= 20b and <= 34b) Models ***"
 
 export LOGFILES=$HOME/Logfiles
 mkdir --parents $LOGFILES
 
 for model in \
   codellama:34b-instruct-q4_0 \
-  deepseek-r1:32b-qwen-distill-q4_K_M \
-  glm-4.7-flash:q4_K_M \
   granite-code:34b-base-q4_0 \
   granite-code:34b-instruct-q4_0 \
-  nemotron-3-nano:30b-a3b-q4_K_M \
+  wizardcoder:33b-v1.1-q4_0 \
   qwen3-vl:32b-thinking-q4_K_M \
   qwen3-vl:32b-instruct-q4_K_M \
   qwen3-coder:30b-a3b-q4_K_M \
-  wizardcoder:33b
+  glm-4.7-flash:q4_K_M \
+  devstral-small-2:24b-instruct-2512-q4_K_M \
+  magistral:24b-small-2506-q4_K_M \
+  codestral:22b-v0.1-q4_0 \
+  gpt-oss:20b
 
 do
   bname=$(basename $model)
 
   echo ""
-  echo "checking $model with ChucK"
-  /usr/bin/time ollama run $model < prompt.ck > $bname.chuck
-
-  echo "checking $model with R"
-  /usr/bin/time ollama run $model < prompt.R > $bname.data.table
+  echo "pulling $model"
+  /usr/bin/time ollama pull $model
 
 done
 
 ollama list | sort -nr -k 3
 
-echo "*** Finished Large (>= 16 GB and <= 24 GB) Models ***"
+echo "*** Finished Large (>= 20b and <= 34b) Models ***"
 echo ""
